f524ca604ba081710a7a1e535ef52550d104ac65
vathana81 <112860457+vathana81@users.noreply.github.com>
2022-09-08 15:00:37 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  8 14:16:12 2022
+
+@author: Administrateur
+"""
+
+from flask import Flask, render_template, request, send_file, redirect, url_for, Response, redirect
+
+app = Flask(__name__)
+
+import sys
+import os
+
+@app.route('/', methods=['GET', 'POST'])
+
+def Menu():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8081)
